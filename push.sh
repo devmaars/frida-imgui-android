9c5110de26f77e7f6859dbee4abe71a59164054a
devmaars <osg1532@gmail.com>
2024-05-31 09:21:34 +0200

    👷 add simple helper script

---
+++ push.sh
@@ -0,0 +1,11 @@
+LIB=build/arm64-v8a/libimgui.so
+# LIB=build/armeabi-v7a/libimgui.so
+DEX=build/java/renderer.dex
+
+adb push $LIB /data/local/tmp
+adb push $DEX /data/local/tmp
+
+adb shell chmod 777 /data/local/tmp/libimgui.so
+adb shell chmod 777 /data/local/tmp/renderer.dex
+
+adb shell setenforce 0
