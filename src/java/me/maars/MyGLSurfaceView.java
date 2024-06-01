3eb8d706ecfb0c48b91259d5b272574d0252959b
devmaars <osg1532@gmail.com>
2024-06-01 12:55:14 +0200

    ♻️ refactor code

--- src/java/me/maars/MyGLSurfaceView.java
+++ src/java/me/maars/MyGLSurfaceView.java
@@ -1,16 +1,28 @@
 package me.maars;
 
+import android.app.Activity;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.Log;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.WindowManager;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 
+import static android.view.WindowManager.LayoutParams;
+import static android.graphics.PixelFormat.TRANSLUCENT;
 import static android.graphics.PixelFormat.TRANSPARENT;
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.WindowManager.LayoutParams.TYPE_APPLICATION;
+import static android.view.WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+import static android.view.WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
 
 public class MyGLSurfaceView extends GLSurfaceView implements GLSurfaceView.Renderer {
-    protected final static String TAG = "MyGLSurfaceView";
+    private final static String TAG = "MyGLSurfaceView";
     private final static String TMP_PATH = "/data/local/tmp";
     private final static String LIB_NAME = "libimgui.so";
 
@@ -30,13 +42,15 @@ public class MyGLSurfaceView extends GLSurfaceView implements GLSurfaceView.Rend
         getHolder().setFormat(TRANSPARENT);
         setZOrderOnTop(true);
         setRenderer(this);
+
+        startMenu(ctx);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         Log.d(TAG, "onSurfaceCreated");
 
-        nativeOnSurfaceCreated();
+        nativeOnSurfaceCreated(getHolder().getSurface());
     }
 
     @Override
@@ -57,14 +71,54 @@ public class MyGLSurfaceView extends GLSurfaceView implements GLSurfaceView.Rend
     public boolean onTouchEvent(MotionEvent event) {
         Log.d(TAG, "onTouchEvent");
 
-        return handleTouch(event.getX(), event.getY(), event.getAction());
+        if (handleTouch(event.getX(), event.getY(), event.getAction()))
+            return true;
+
+        // If ImGui doesn't handle the event, dispatch it to the underlying views
+        View rootView = ((Activity) getContext()).getWindow().getDecorView().getRootView();
+        return dispatchTouchEventToRoot(rootView, event);
     }
 
-    public static native void nativeOnDrawFrame();
+    private boolean dispatchTouchEventToRoot(View rootView, MotionEvent event) {
+        if (rootView instanceof ViewGroup) {
+            ViewGroup rootViewGroup = (ViewGroup) rootView;
+            MotionEvent eventCopy = MotionEvent.obtain(event);
 
-    public static native void nativeOnSurfaceChanged(int width, int height);
+            for (int i = 0; i < rootViewGroup.getChildCount(); i++) {
+                View child = rootViewGroup.getChildAt(i);
+                if (child != this && child.dispatchTouchEvent(eventCopy))
+                    return true;
+            }
+        }
+        return false;
+    }
 
-    public static native void nativeOnSurfaceCreated();
+    private void startMenu(Context ctx) {
+        Log.d(TAG, "startMenu");
 
-    public static native boolean handleTouch(float x, float y, int action);
+        // check if the view is already added
+        if (getParent() != null) {
+            return;
+        }
+
+        WindowManager wm = ((Activity) ctx).getWindowManager();
+        LayoutParams params = new LayoutParams(
+                MATCH_PARENT,
+                MATCH_PARENT,
+                TYPE_APPLICATION,
+                FLAG_NOT_TOUCH_MODAL | FLAG_NOT_FOCUSABLE,
+                TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL;
+
+        wm.addView(this, params);
+    }
+
+    private static native void nativeOnDrawFrame();
+
+    private static native void nativeOnSurfaceChanged(int width, int height);
+
+    private static native void nativeOnSurfaceCreated(Surface surface);
+
+    private static native boolean handleTouch(float x, float y, int action);
 }
