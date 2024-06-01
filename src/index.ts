fccd2b3dfdd4e49e6676284358de713dd2fd979c
devmaars <osg1532@gmail.com>
2024-06-02 01:06:47 +0200

    ⚗️ perform experiment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { log, sleep } from './util.js';
 
-console.log("Script loaded successfully 🚀")
+console.log('Script loaded successfully 🚀');
 
 const TMP_DIR = '/data/local/tmp';
+const LIB_IMGUI_PATH = TMP_DIR + '/libimgui.so';
 const RENDERER_DEX_PATH = TMP_DIR + '/renderer.dex';
 
 Java.perform(() => {
@@ -10,12 +11,80 @@ Java.perform(() => {
 });
 
 async function main() {
+  console.log(`Process id: ${Process.id}`);
+
+  const System = Java.use('java.lang.System');
+
+  // const libimgui = Module.load(LIB_IMGUI_PATH);
+
+  System.load(LIB_IMGUI_PATH);
   Java.openClassFile(RENDERER_DEX_PATH).load();
+
+  // @ts-ignore
+  const maps = File.readAllText('/proc/self/maps')
+    .split('\n')
+    .filter((line: string) => line.includes('libimgui'));
+  console.log(maps);
+
+  // let do_dlopen = NULL;
+  // let call_constructor = NULL;
+  // Process.findModuleByName('linker64') // change to linker32 if needed for 32-bit
+  //   ?.enumerateSymbols()
+  //   .forEach(function (symbol) {
+  //     if (symbol.name.indexOf('do_dlopen') >= 0) {
+  //       do_dlopen = symbol.address;
+  //     }
+  //     if (symbol.name.indexOf('call_constructor') >= 0) {
+  //       call_constructor = symbol.address;
+  //     }
+  //   });
+
+  // let lib_loaded = 0;
+  // // let libimgui = null;
+  // Interceptor.attach(do_dlopen, function () {
+  //   // @ts-ignore
+  //   const library_path = this.context.x0.readCString();
+  //   console.log(`[+] Loading library: ${library_path}`);
+  //   if (library_path.indexOf('libimgui') >= 0) {
+  //     Interceptor.attach(call_constructor, function () {
+  //       if (lib_loaded == 0) {
+  //         lib_loaded = 1;
+  //         // libimgui = Process.findModuleByName('libimgui.so');
+  //         // console.log(`[+] libimgui is loaded at ${libimgui?.base}`);
+  //       }
+  //     });
+  //   }
+  // });
+
+  // const __cxa_demangle = libimgui.getExportByName('__cxa_demangle');
+  // const Demangle = new NativeFunction(__cxa_demangle, 'pointer', ['pointer', 'pointer', 'pointer', 'pointer']);
+
+  // const demangledStatus = Memory.alloc(0x4);
+  // function findImGuiSymbol(name: string) {
+  //   const symbol = libimgui.enumerateExports().find((exp) => {
+  //     const expName = Memory.allocUtf8String(exp.name);
+  //     const realName = Demangle(expName, NULL, NULL, demangledStatus);
+  //     if (demangledStatus.readU32() !== 0) return false;
+
+  //     return realName.readCString()?.includes(name);
+  //   });
+
+  //   return symbol;
+  // }
+
+  //TODO: check /proc/self/maps for the address of libimgui.so
+
+  // function getImGuiSymbol(name: string) {
+  //   const symbol = findImGuiSymbol(name);
+  //   if (!symbol) throw new Error(`Symbol ${name} not found`);
+
+  //   return symbol.address;
+  // }
 
   const Activity = Java.use('android.app.Activity');
   const MyGLSurfaceView = Java.use('me.maars.MyGLSurfaceView');
 
-  console.log(MyGLSurfaceView)
+  console.log(MyGLSurfaceView);
 
   let rendererSet = false;
   let glSurfaceView: Java.Wrapper;
@@ -24,22 +93,32 @@ async function main() {
     log('Activity.onCreate()');
     this.onCreate(savedInstanceState);
 
-    // Check OpenGL ES version before creating GLSurfaceView
-    if (!supportsOpenGLES3()) {
-      console.error("This device does not support OpenGL ES 3.0");
-      return;
-    }
-
     glSurfaceView = MyGLSurfaceView.$new(this);
     glSurfaceView = Java.retain(glSurfaceView);
-    console.log(glSurfaceView)
+    console.log(glSurfaceView);
 
     rendererSet = true;
   };
 
-  // Function to check if the device supports OpenGL ES 3.0
-  function supportsOpenGLES3() {
-    const GLES30 = Java.use('android.opengl.GLES30');
-    return GLES30.$new() !== null;
-  }
+  // const ImGui = {
+  //   Begin: new NativeFunction(getImGuiSymbol('Begin'), 'bool', ['pointer']),
+  //   End: new NativeFunction(getImGuiSymbol('End'), 'void', []),
+  //   Text: new NativeFunction(getImGuiSymbol('Text'), 'void', ['pointer']),
+  // };
+
+  // const renderFrame = new NativeFunction(libimgui.getExportByName('renderFrame'), 'void', []);
+  // console.log('renderFrame', renderFrame);
+
+  // Interceptor.replace(
+  //   renderFrame,
+  //   new NativeCallback(
+  //     () => {
+  //       ImGui.Begin(Memory.allocUtf8String('Hello, world! From Frida 🚀'));
+  //       ImGui.Text(Memory.allocUtf8String('Welcome to the ImGui demo!'));
+  //       ImGui.End();
+  //     },
+  //     'void',
+  //     [],
+  //   ),
+  // );
 }
