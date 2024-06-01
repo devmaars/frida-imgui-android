710c6260d04976ffc23734bd86474ef4da45c981
devmaars <osg1532@gmail.com>
2024-06-01 08:41:01 +0200

    🩹 simple fix imgui window size

--- src/cpp/imgui/imgui_impl_android.h
+++ src/cpp/imgui/imgui_impl_android.h
@@ -22,15 +22,15 @@
 // - Introduction, links and more at the top of imgui.cpp
 
 #pragma once
-#include "imgui.h"      // IMGUI_IMPL_API
+#include "imgui.h" // IMGUI_IMPL_API
 #ifndef IMGUI_DISABLE
 
 struct ANativeWindow;
 struct AInputEvent;
 
-IMGUI_IMPL_API bool     ImGui_ImplAndroid_Init(ANativeWindow* window);
-IMGUI_IMPL_API int32_t  ImGui_ImplAndroid_HandleInputEvent(const AInputEvent* input_event);
-IMGUI_IMPL_API void     ImGui_ImplAndroid_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplAndroid_NewFrame();
+IMGUI_IMPL_API bool ImGui_ImplAndroid_Init(ANativeWindow *window);
+IMGUI_IMPL_API int32_t ImGui_ImplAndroid_HandleInputEvent(const AInputEvent *input_event);
+IMGUI_IMPL_API void ImGui_ImplAndroid_Shutdown();
+IMGUI_IMPL_API void ImGui_ImplAndroid_NewFrame(int width, int height);
 
 #endif // #ifndef IMGUI_DISABLE
